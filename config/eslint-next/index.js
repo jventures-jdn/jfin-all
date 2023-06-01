4d8d499cad10f0cb53eb747accb3c9846275058f
Vasin Sermsampan <dev.vasin@gmail.com>
2023-06-01 16:23:08 +0700

    perf(config): update config
    - `eslint` add nex-babel for prevent tsconfig error
    - `postcss` add thailwindcss/nesting
    - `tailwind` update theme & add daisy ui
    - `typescript` update hardhat nextjs

--- config/eslint-next/index.js
+++ config/eslint-next/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['turbo', 'next/core-web-vitals', 'prettier', 'next'],
+    extends: ['turbo', 'next/core-web-vitals', 'prettier', 'next', 'next-babel'],
     ignorePatterns: ['node_modules', 'dist', 'out', '.next'],
     rules: {
         '@next/next/no-html-link-for-pages': 'off',
--- config/tailwind/index.js
+++ config/tailwind/index.js
@@ -4,12 +4,7 @@ module.exports = {
     mode: 'jit',
     theme: {
         extend: {
-            colors: {
-                primary: { 400: '#3e33be', 800: '#282640' },
-                secondary: { 400: '#fcd201' },
-                background: '',
-                danger: '#c92229',
-            },
+            colors: {}, // extends color class
         },
         container: {
             center: true,
@@ -22,5 +17,28 @@ module.exports = {
             },
         },
     },
-    plugins: [require('@tailwindcss/typography')],
+    daisyui: {
+        themes: [
+            {
+                jfinTheme: {
+                    primary: '#c92229',
+                    'primary-focus': '#ed000050',
+                    'primary-content': '#ffffff',
+                    secondary: '#3e33be',
+                    'base-300': '#0b0d0f',
+                    'base-200': '#16191d',
+                    'base-100': '#2e3338',
+                },
+            },
+        ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+        darkTheme: 'jfinTheme', // name of one of the included themes for dark mode
+        base: false, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+        prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    },
+
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
