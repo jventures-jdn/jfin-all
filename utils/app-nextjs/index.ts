d85fcdfa30d4e14a789010ebadfa8a5d33177034
Vasin Sermsampan <dev.vasin@gmail.com>
2023-06-02 17:18:35 +0700

    refactor(app-nextjs): add types

--- utils/app-nextjs/index.ts
+++ utils/app-nextjs/index.ts
@@ -1,3 +1,4 @@
 export { useNavigator } from './src/helpers/use-navigator'
 export { useUrlQueryRemover } from './src/helpers/use-url-query-remover'
+export * from './src/types/common'
 export { default as NoSSR } from 'react-no-ssr'
