06dbe48f08a18fe7b7ee9332247fd8417a4879dc
Vasin Sermsampan <dev.vasin@gmail.com>
2023-06-12 16:37:30 +0700

    chore(*): remove old repo

--- libs/staking-react/index.ts
+++
@@ -1,4 +0,0 @@
-export * as chain from "./src/chain";
-export * as contract from "./src/contract";
-export * as gas from "./src/gas";
-export * as wallet from "./src/utils/wallet";
--- libs/staking-react/src/gas/index.ts
+++
@@ -1,4 +0,0 @@
-export const GAS_LIMIT_CLAIM = process.env.NETWORK === 'jfintest' ? '7000000' : '25000000'
-export const GAS_LIMIT_GOVERNANCE = process.env.NETWORK === 'jfintest' ? '7000000' : '15000000'
-export const GAS_LIMIT_GENERAL = process.env.NETWORK === 'jfintest' ? '1000000' : '1000000' // use in case some sned transaction cannot estimate gas
-export const GAS_PRICE = BigInt(23000000000)
