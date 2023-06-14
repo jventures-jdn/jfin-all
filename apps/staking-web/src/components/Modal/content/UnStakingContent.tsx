ba3e78ff5c47eba7ec304849fab7e76044d74f75
Vasin Sermsampan <dev.vasin@gmail.com>
2023-06-14 21:25:42 +0700

    perf(staking-web): migrate UnStakingContent modal
    - update un-stake message
    - migrate modal to support wagmi 1.0
    - for #66 #67

--- apps/staking-web/src/components/Modal/content/UnStakingContent.tsx
+++ apps/staking-web/src/components/Modal/content/UnStakingContent.tsx
@@ -5,6 +5,10 @@ import { getCurrentEnv, useModalStore } from '../../../stores'
 import { message } from 'antd'
 import JfinCoin from '../../JfinCoin/JfinCoin'
 import { Validator, chainStaking } from '@utils/staking-contract'
+import { Address } from 'wagmi'
+import { formatEther } from 'ethers/lib/utils'
+import { EXPECT_CHAIN } from '@utils/chain-config'
+import { Link } from 'react-router-dom'
 
 interface IUnStakingContent {
   forceActionButtonsEnabled?: boolean
@@ -28,7 +32,6 @@ const UnStakingContent = observer((props: IUnStakingContent) => {
     e.preventDefault()
     setError(undefined)
 
-    console.log(props.forceActionButtonsEnabled)
     if (!props.forceActionButtonsEnabled) {
       if (unStakingAmount < 1) return setError('Un-Stake amount must be more 1')
       if (unStakingAmount > Number(stakedAmount))
@@ -40,7 +43,7 @@ const UnStakingContent = observer((props: IUnStakingContent) => {
     try {
       modalStore.setIsLoading(true)
       await chainStaking.unstakeFromValidator(
-        props.validator.ownerAddress,
+        props.validator.owner as Address,
         unStakingAmount,
       )
       modalStore.setVisible(false)
@@ -54,11 +57,10 @@ const UnStakingContent = observer((props: IUnStakingContent) => {
 
   const initial = async () => {
     modalStore.setIsLoading(true)
-    setStakedAmount(
-      (
-        await chainStaking.getMyStakingAmount(props.validator.ownerAddress)
-      ).toNumber(),
+    const myStakingAmount = Number(
+      formatEther(await chainStaking.getMyStakingAmount(props.validator.owner)),
     )
+    setStakedAmount(myStakingAmount)
     modalStore.setIsLoading(false)
   }
 
@@ -95,9 +97,14 @@ const UnStakingContent = observer((props: IUnStakingContent) => {
 
         <div className="warning-message">
           <WarningOutlined />
-          After complete the transaction, your staking will be returned in the
-          form of a staking reward within 1 Epoch (
-          {getCurrentEnv() === 'jfin' ? '1hr' : '10min'}).
+          After you unstake, jfin will be sent to you in the form of rewards.
+          which you will have to wait for 1 Epoch (
+          {EXPECT_CHAIN.chainNetwork == 'JFIN' ? '1hr' : '10min'}), <br />
+          Please see the{' '}
+          <a href="/assets" target="_blank">
+            Assets
+          </a>{' '}
+          page for more details.
         </div>
 
         <button
