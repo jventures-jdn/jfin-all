1955f84292c774f4ad3fd34d7290948fdb3ba40c
naripsaejan <naripsaejan@gmail.com>
2023-06-21 16:12:34 +0700

    fix(explorer-web): add comment

--- apps/jfin-explorer-web/components/stats.tsx
+++ apps/jfin-explorer-web/components/stats.tsx
@@ -1,18 +1,21 @@
 import { useBlockscout } from '@libs/blockscout-client-react'
 
 export function StatsComponentDemo() {
+    // Look for total_blocks number
+    // const { data } = useBlockscout().stats().meta()
+
     // Get stats data, this will auto fetch if data not exist
+    // const stats = useBlockscout().stats().get(data?.total_blocks)
     const stats = useBlockscout().stats().get({ scrape: true })
 
-    // console.log('txs__ stats', stats?.data)
     if (stats?.isLoading) return <span>Loading...</span>
 
     return (
         <div>
-            <div>Average block time:{stats?.data?.average_block_time}</div>
+            <div>Average block time:{stats?.data.average_block_time}</div>
             <div>Total transactions:{stats?.data?.total_transactions}</div>
             <div>Total blocks:{stats?.data?.total_blocks}</div>
-            <div>Wallet addresses:{stats?.data?.total_addresses}</div>
+            <div>Wallet addresses:{stats?.data.total_addresses}</div>
         </div>
     )
 }
