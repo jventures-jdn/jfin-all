8d32306ac3d05f0436238e685528df95d6dd4df5
naripsaejan <naripsaejan@gmail.com>
2023-06-28 17:06:40 +0700

    feat(jfin-explorer-web):call api blocks for txs

--- apps/jfin-explorer-web/app/txs/page.tsx
+++ apps/jfin-explorer-web/app/txs/page.tsx
@@ -1,3 +1,68 @@
+'use client'
+
+import { useBlockscout } from '@libs/blockscout-client-react'
+import { BlocksPageListComponent } from '../../components/block'
+import { Button } from '@chakra-ui/react'
+
 export default function ValidatedTransactionsPage() {
-    return <div>Validated Transactions</div>
+    const {
+        list,
+        blockNumber,
+        itemCount,
+        pageIndex,
+        isFirstPage,
+        isLastPage,
+        isWs,
+        isValidBlock,
+        nextPage,
+        previousPage,
+    } = useBlockscout().blocks().list()
+
+    return (
+        <div id="blocks-page">
+            <div>Validated Transactions</div>
+
+            <br />
+            <div>Page Index : {pageIndex}</div>
+            <div>Block Number : {blockNumber}</div>
+            {isWs && <WebSocket />}
+            <div className="flex">
+                <Button
+                    isDisabled={isFirstPage || list?.isLoading}
+                    onClick={nextPage}
+                    colorScheme="teal"
+                    size="xs"
+                    margin="1"
+                >
+                    Next
+                </Button>
+                <Button
+                    isDisabled={isLastPage || list?.isLoading}
+                    onClick={previousPage}
+                    colorScheme="teal"
+                    size="xs"
+                    margin="1"
+                >
+                    Previous
+                </Button>
+            </div>
+            {!isValidBlock && <div>There are no blocks. </div>}
+            {list?.isLoading && isValidBlock && <>Loading...</>}
+            {!list?.isLoading && isValidBlock && (
+                <div>
+                    Blocks :
+                    <BlocksPageListComponent count={itemCount} />
+                </div>
+            )}
+            <br />
+        </div>
+    )
+}
+
+function WebSocket() {
+    const ws = useBlockscout().webSocket({
+        newBlocks: true,
+    })
+
+    return <div>WebSocket : {ws.connectionStatus}</div>
 }
