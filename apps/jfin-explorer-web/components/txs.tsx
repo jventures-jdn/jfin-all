276bb34b1ad471a3a7b4199bce85bf7368a955ad
naripsaejan <naripsaejan@gmail.com>
2023-07-11 15:05:08 +0700

    feat(explorer-web): update txs

--- apps/jfin-explorer-web/components/txs.tsx
+++ apps/jfin-explorer-web/components/txs.tsx
@@ -6,6 +6,7 @@ export function BlockComponentDemo(props: { blockNumber: number; scrape?: boolea
     // Get block data, this will auto fetch if data not exist
     const block = useBlockscout().txs().get(blockNumber, { scrape })
     if (block.isLoading) return <span>Loading...</span>
+    console.log('get_component')
     return (
         <div>
             <Link href={`/block/${blockNumber}`} prefetch={false}>
