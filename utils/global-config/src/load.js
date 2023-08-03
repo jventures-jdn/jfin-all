4793e7e73c7ce76a86ec782192c69954a5f96f95
Nick <thitipong.nick@gmail.com>
2023-08-03 23:02:06 +0700

    auto use default global config

--- utils/global-config/src/load.js
+++ utils/global-config/src/load.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
 const path = require('path')
 const { yaml, yamls } = require('@utils/js-utilities/index_')
+const defaultGlobalConfig = require('./default_')
 
 // Load global .config.yaml to json object, for builders
 module.exports = toProcessEnv => {
     const globalConfigPath = path.join(process.cwd(), `../../.config.yaml`)
     const exists = fs.existsSync(globalConfigPath)
-    if (!exists) throw Error('⛔ Global config file not found')
-    const result = yaml.load(fs.readFileSync(globalConfigPath).toString())
+    let result
+    if (!exists) {
+        console.log('🔎 Global config file not found, auto use default')
+        result = defaultGlobalConfig
+    } else {
+        result = yaml.load(fs.readFileSync(globalConfigPath).toString())
+    }
     const minified = yamls.YamlUtilities.minify(result)
     if (toProcessEnv === true) {
         process.env.GLOBAL_CONFIG = minified
