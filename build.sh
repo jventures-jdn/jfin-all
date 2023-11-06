72fb74395203a50a95ea12cb4c3f8188bef9d169
Vasin Sermsampan <dev.vasin@gmail.com>
2023-11-06 13:13:53 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -24,6 +24,9 @@ if [[ $CF_PAGES_BRANCH =~ "staking" ]]; then
         fi
     fi
 else
-    echo Error : project not support
-    exit 1
+    if [[ $CF_PAGES_BRANCH =~ "staking" ]]; then
+         pnpm -C apps/staking-web build:jfin --outDir=../../dist/output/static
+    else 
+        echo Error : project not support
+        exit 1
 fi
