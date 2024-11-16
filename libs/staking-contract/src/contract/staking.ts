67d50c1d5e15ab35ed2f223d7b9990f6385316d0
Vasin Sermsampan <dev.vasin@gmail.com>
2024-11-16 12:02:39 +0700

    fix(staking-contract): add gas limit to stakeToValidator

--- libs/staking-contract/src/contract/staking.ts
+++ libs/staking-contract/src/contract/staking.ts
@@ -460,6 +460,7 @@ export class Staking {
             const execute: Promise<Address> = new Promise((resolve, reject) => {
                 return contract.write
                     .delegate([validatorAddress], {
+                        gas: CHAIN_GAS_LIMIT_CUSTOM[EXPECT_CHAIN.chainNetwork].stake,
                         gasPrice: CHAIN_GAS_PRICE[EXPECT_CHAIN.chainNetwork],
                         value: parseEther(`${amount}`),
                     })
